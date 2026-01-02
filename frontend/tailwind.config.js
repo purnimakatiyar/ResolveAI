15eab42b065e87c504b230b04f79abaf9ea49b24
purnimakatiyar <purnimakatiyar48@gmail.com>
2026-01-03 01:02:51 +0530

    added ui for auth and fixed tailwind setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class", // ✅ REQUIRED
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
